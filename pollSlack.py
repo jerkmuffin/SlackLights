7d2e0f2c4dfd2f59a4efddddf0a7b939534cae38
mike schroffel <imikes@gmail.com>
2021-04-17 18:50:25 -0700

    shhh

--- pollSlack.py
+++ pollSlack.py
@@ -2,13 +2,13 @@ import json
 import requests
 import time
 import RPi.GPIO as rpi
-
+import secrets
 rpi.setmode(rpi.BOARD)
 rpi.setup(38, rpi.OUT)
 rpi.setup(40, rpi.OUT)
 
 
-TOKEN="xoxp-331483125874-330826891536-1911290309109-428e0f1a6212564df056a95790a2d60f"
+TOKEN = secrets.TOKEN
 
 url = 'https://slack.com/api/users.profile.get?pretty=1'
 headerer = {'Authorization': 'Bearer ' + TOKEN}
